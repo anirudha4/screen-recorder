154cf82c05e437d73cdd26e669dc83d9a8e25fcf
Anirudha Gandhare <anirudha@chisellabs.com>
2024-06-10 12:56:51 +0530

    feat: ffmpeg - convert video to gif

--- src/components/screen-recorder.tsx
+++ src/components/screen-recorder.tsx
@@ -1,27 +1,57 @@
 "use client";
-import { displayMediaOptions } from "@/lib/constants";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { useState } from "react";
-import { Alert, AlertTitle } from "./ui/alert";
-import { Video } from "lucide-react";
-import { ExclamationTriangleIcon } from "@radix-ui/react-icons"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
+import { toBlobURL } from "@ffmpeg/util";
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { displayMediaOptions } from "@/lib/constants";
+import { processVideo } from "@/lib/utils";
+import ScreenLoader from "./screen-loader";
+
 
 
 type Props = {}
 const ScreenRecorder = ({ }: Props) => {
+    const [_, setLoaded] = useState(false);
+    const ffmpegRef = useRef(new FFmpeg());
+    const [activeTab, setActiveTab] = useState('gif');
+
     const router = useRouter();
     const [conf, setConf] = useState({
         fileName: '',
         format: '.webm'
     })
-    const [error, setError] = useState('');
-    // const [success, setSuccess] = useState('');
     const [loading, setLoading] = useState(false);
     const [isRecording, setIsRecording] = useState(false);
-    let recorder = null
+    let recorder = null;
+
+    // load ffmpeg wasm
+    const load = async () => {
+        const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd'
+        const ffmpeg = ffmpegRef.current;
+
+        try {
+            await ffmpeg.load({
+                coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
+                wasmURL: await toBlobURL(
+                    `${baseURL}/ffmpeg-core.wasm`,
+                    "application/wasm"
+                ),
+                workerURL: await toBlobURL(
+                    `${baseURL}/ffmpeg-core.worker.js`,
+                    "text/javascript"
+                ),
+            });
+            setLoaded(true);
+        } catch (error: any) {
+            console.log("error", error.message);
+
+        }
+    };
+
     async function startCapture(displayMediaOptions: DisplayMediaStreamOptions | undefined) {
         let stream = null;
 
@@ -38,39 +68,9 @@ const ScreenRecorder = ({ }: Props) => {
             }
 
             recorder.onstop = async () => {
-                let data = null;
-                /**
-                 * Not the best way.
-                 * @todo - Needs optimization
-                 */
-                // for (const chunk of chunks) {
-                const type = conf.format.split('.')[1]
-                const videoBlob = new Blob(chunks, { type: `video/${type}` });
-                const a = document.createElement('a');
-                a.href = URL.createObjectURL(videoBlob);
-                a.download = conf.fileName + conf.format;
-                a.click();
-
-                const formData = new FormData();
-                formData.append('video', videoBlob);
-                formData.append('fileName', conf.fileName);
-                formData.append('format', conf.format);
                 setLoading(true);
-
-                // const response = await fetch('/api/record', {
-                //     method: 'POST',
-                //     body: formData
-                // });
-                // data = await response.json();
-                // }
-                /**
-                 * currently we are only handling the last request, if it is successful or not
-                 * @todo - We should check for each chunk request and do a rerequest
-                 */
-                // if (data.status !== 'success') {
-                //     return setError('Something went wrong. Please try again later. Thank You!')
-                // }
-                // setSuccess(data.videoUrl)
+                const isGif = activeTab === 'gif'
+                await processVideo(chunks, conf.fileName, conf.format, ffmpegRef.current, isGif);
                 setLoading(false);
                 setConf({ ...conf, fileName: '' })
                 setIsRecording(false)
@@ -78,7 +78,6 @@ const ScreenRecorder = ({ }: Props) => {
             }
             recorder.start(200)
         } catch (err) {
-            console.error(`Error: ${err}`);
             setIsRecording(false)
         }
         return stream;
@@ -86,44 +85,56 @@ const ScreenRecorder = ({ }: Props) => {
 
     const handleStartRecording = async () => startCapture(displayMediaOptions);
 
+    useEffect(() => {
+        load()
+    }, []);
     return (
         <div className="max-w-[600px] w-full rounded-md border bg-white">
-            <p className="font-semibold text-slate-700 p-4 flex items-center gap-3">
-                <Video size={22} />
-                Screen Recorder
-            </p>
-            <div className="grid items-center grid-cols-2 gap-4 p-4 border-t">
-                <Input
-                    placeholder="Enter recording name. "
-                    value={conf.fileName}
-                    onChange={e => setConf({ ...conf, fileName: e.target.value })}
-                    className="w-full"
-                />
-                <Select value={conf.format} onValueChange={value => setConf({ ...conf, format: value })}>
-                    <SelectTrigger className="w-full">
-                        <SelectValue />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value=".mp4">MP4</SelectItem>
-                        <SelectItem value=".mkv">MKV</SelectItem>
-                        <SelectItem value=".webm">WEBM</SelectItem>
-                    </SelectContent>
-                </Select>
-                <Button className="col-span-2" disabled={!conf.fileName || loading || isRecording} onClick={handleStartRecording} size={'sm'}>
-                    {loading ? 'Saving Recording' : 'Start Recording'}
-                </Button>
-            </div>
-            {error && (
-                <div className="p-4 border-t text-sm">
-                    <Alert variant="destructive" className="mt-3">
-                        <ExclamationTriangleIcon className="h-4 w-4" />
-                        <AlertTitle>Oops!</AlertTitle>
-                <AlertTitle>
-                            {error}
-                        </AlertTitle>
-                    </Alert>
+            {loading && <ScreenLoader message={`Saving ${conf.fileName}${activeTab === 'gif' ? '.gif' : conf.format}`} />}
+            <Tabs defaultValue="gif" className="w-full" >
+                <div className="px-2 pt-2">
+                    <TabsList className="w-full">
+                        <TabsTrigger onClick={_ => setActiveTab('gif')} className="flex-1" value="gif">GIF Recorder</TabsTrigger>
+                        <TabsTrigger onClick={_ => setActiveTab('video')} className="flex-1" value="video">Video Recorder</TabsTrigger>
+                    </TabsList>
                 </div>
-            )}
+                <TabsContent value="video">
+                    <div className="grid items-center grid-cols-2 gap-4 p-4 border-t">
+                        <Input
+                            placeholder="Enter recording name. "
+                            value={conf.fileName}
+                            onChange={e => setConf({ ...conf, fileName: e.target.value })}
+                            className="w-full"
+                        />
+                        <Select value={conf.format} onValueChange={value => setConf({ ...conf, format: value })}>
+                            <SelectTrigger className="w-full">
+                                <SelectValue />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value=".mp4">MP4</SelectItem>
+                                <SelectItem value=".mkv">MKV</SelectItem>
+                                <SelectItem value=".webm">WEBM</SelectItem>
+                            </SelectContent>
+                        </Select>
+                        <Button className="col-span-2" disabled={!conf.fileName || loading || isRecording} onClick={handleStartRecording} size={'sm'}>
+                            {loading ? 'Saving Recording' : 'Start Recording'}
+                        </Button>
+                    </div>
+                </TabsContent>
+                <TabsContent value="gif">
+                    <div className="grid items-center grid-cols-1 gap-4 p-4 border-t">
+                        <Input
+                            placeholder="Enter GIF name"
+                            value={conf.fileName}
+                            onChange={e => setConf({ ...conf, fileName: e.target.value })}
+                            className="w-full"
+                        />
+                        <Button disabled={!conf.fileName || loading || isRecording} onClick={handleStartRecording} size={'sm'}>
+                            {loading ? 'Saving Recording' : 'Start Recording'}
+                        </Button>
+                    </div>
+                </TabsContent>
+            </Tabs>
         </div >
     )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,53 @@
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export async function processVideo(chunks: any, fileName: string, format: string, ffmpeg: FFmpeg, isGif: boolean) {
+  const type = format.split('.')[1]
+  let buffer: any = new Blob(chunks, { type: `video/${type}` });
+
+  /**
+   * convert video to gif using ffmpeg
+   */
+  const outputName = fileName + (isGif ? '.gif' : format);
+  if (isGif) {
+    const name = fileName + format;
+    await ffmpeg.writeFile(name, await fetchFile(buffer));
+    await ffmpeg.exec(["-i", name, outputName, "-r", '24']);
+    const fileData = await ffmpeg.readFile(outputName);
+    const data = new Uint8Array(fileData as ArrayBuffer);
+    buffer = new Blob([data.buffer])
+  }
+
+
+  /**
+   * locally download the recorded file
+   */
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(buffer);
+  a.download = outputName;
+  a.click();
+
+  /**
+   * upload video/gif to supabase
+   */
+  // const formData = new FormData();
+  // formData.append('video', videoBlob);
+  // formData.append('fileName', fileName);
+  // formData.append('format', format);
+
+  // const response = await fetch('/api/record', {
+  //     method: 'POST',
+  //     body: formData
+  // });
+  // const data = await response.json();
+  // }
+  // return data;
+
 }
